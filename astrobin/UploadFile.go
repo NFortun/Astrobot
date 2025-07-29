ffaf94940ad5d582421da5611dbad72b83be8199
NFortun <nicolas.fortun.nageotte@outlook.fr>
2025-07-29 19:45:20 +0200

    fix(reader): copy io.Reader

--- astrobin/UploadFile.go
+++ astrobin/UploadFile.go
@@ -1,8 +1,10 @@
 package astrobin
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/go-openapi/runtime/middleware"
@@ -23,14 +25,26 @@ func UploadFile(params api.UploadImageParams) middleware.Responder {
 	fmt.Printf("content type: %s\n", params.HTTPRequest.Header.Get("Content-Type"))
 
 	uploaders := []uploader.Uploader{&uploader.Drive{}, &uploader.Instagram{}}
+	file := params.UpFile
+	data, err := io.ReadAll(file)
+	if err != nil {
+		msg := fmt.Sprintf("fail to read data: %s", err.Error())
+		return api.NewUploadImageDefault(http.StatusInternalServerError).WithPayload(&models.Error{Message: &msg})
+	}
+
+	var caption string
+	if params.Caption != nil {
+		caption = *params.Caption
+	}
+
 	for _, uploader := range uploaders {
-		file := params.UpFile
+		reader := bytes.NewReader(data)
 		if err := uploader.Connect(ctx); err != nil {
 			msg := fmt.Sprintf("fail to connect: %s", err.Error())
 			return api.NewUploadImageDefault(http.StatusInternalServerError).WithPayload(&models.Error{Message: &msg})
 		}
 
-		if err := uploader.Upload(ctx, file, params.Name, *params.Caption); err != nil {
+		if err := uploader.Upload(ctx, reader, params.Name, caption); err != nil {
 			msg := fmt.Sprintf("fail to upload: %s", err.Error())
 			return api.NewUploadImageDefault(http.StatusInternalServerError).WithPayload(&models.Error{Message: &msg})
 		}
